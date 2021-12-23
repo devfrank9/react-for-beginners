eb7f957cd08875c882e5ca421164edf22664c400
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-23 22:00:01 +0900

    route를 사용하여 파일 세분화 시키기

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setloading] = useState(true);
-  const [movies, setmovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setmovies(json.data.movies);
-    setloading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
